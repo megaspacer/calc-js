dd8daa175032caec3b06bbcd1a162fc2c0f4d298
Maria Kalyugina <megaspacer@gmail.com>
2018-02-06 19:12:03 +0300

    render numbers

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
 // numbers
 const numbers = document.querySelectorAll('.number');
 // output
+const output = document.querySelector('.output');
 const input = document.querySelector('.output > input');
 // operations
 const plus = document.querySelector('#plus');
 const equals = document.querySelector('#equals');
 
-plus.addEventListener('click', function() {
-  input.value += '+';
+plus.addEventListener('click', function () {
+  const div = document.createElement('div');
+  if (!isNaN(input.value)) {
+    div.innerHTML = `<span class="input-numbers">${input.value}</span>`;
+    output.appendChild(div);
+    input.value = '';
+  }
 });
 
-equals.addEventListener('click', function() {
-  
+equals.addEventListener('click', function () {
 });
 
 const addEventForButtons = () => {
   numbers.forEach((number) => {
-    number.addEventListener('click', function() {
+    number.addEventListener('click', function () {
       input.value += this.dataset.value;
     });
   });
